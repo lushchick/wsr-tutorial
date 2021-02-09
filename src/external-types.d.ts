9fc04cb4aebd3a37def742ab8d1590b7fd99434c
Andrii Lushchyk <andriil@wix.com>
2021-02-09 10:15:44 +0200

    Initial commit from Create Yoshi App

---
+++ src/external-types.d.ts
@@ -0,0 +1,18 @@
+/// <reference types="yoshi/types" />
+/// <reference types="jest-yoshi-preset/types" />
+/// <reference types="@wix/wix-bootstrap-renderer/declarations" />
+
+interface Window {
+  __LOCALE__: string;
+  __BASEURL__: string;
+  __MESSAGES__: Messages;
+}
+
+// tslint:disable-next-line:no-namespace
+declare namespace Express {
+  interface Request {
+    aspects: any;
+  }
+}
+
+declare module 'wix-node-i18n-cache';
